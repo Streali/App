437fc09efe745e269ea4009b1d0793013b6b84e0
Romain Lanz <romain.lanz+github@pm.me>
2022-12-29 15:49:44 +0100

    fix(redirect): remove redirect behavior and avoid double flash

--- src/components/protect/protected.tsx
+++ src/components/protect/protected.tsx
@@ -4,13 +4,10 @@ import { useAuth } from '~/contexts/auth-provider';
 // eslint-disable-next-line react/prop-types
 export const Protected = ({ redirectPath = '/login' }) => {
   const { user } = useAuth();
-  const location = useLocation();
 
   if (user) {
     return <Outlet />;
   }
-
-  localStorage.setItem('redirectPath', location.pathname);
 
   return <Navigate to={redirectPath} replace />;
 };
