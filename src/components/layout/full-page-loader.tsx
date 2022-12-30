1363aec22551712c3c90d9a03b6a0b5f9ec38732
Wiliam Traoré <will@mewo.io>
2022-12-30 11:39:47 +0100

    feat(loader): add video loader

--- src/components/layout/full-page-loader.tsx
+++ src/components/layout/full-page-loader.tsx
@@ -1,3 +1,9 @@
+import Loader from '~/assets/loader.webm';
+
 export default function FullPageLoader() {
-  return <p>Loading...</p>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-dark-500">
+      <video src={Loader} autoPlay loop className="w-[300px]"></video>
+    </div>
+  );
 }
