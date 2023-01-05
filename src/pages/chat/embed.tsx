44fee83c2da5b8da5c520a5d711b92bceb576137
Romain Lanz <romain.lanz+github@pm.me>
2023-01-05 23:02:07 +0100

    feat(embeds): increase font size on embed messages

--- src/pages/chat/embed.tsx
+++ src/pages/chat/embed.tsx
@@ -1,7 +1,7 @@
 export default function ChatEmbed() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <p className="rounded bg-error-500 p-3">
+      <p className="rounded bg-error-500 p-3 text-2xl">
         This embed URL is no longer valid. You need to get the new one on app.streali.com
       </p>
     </div>
