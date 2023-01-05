90a855a7dbca9724d93a118e9e1d638d10eab5fc
Romain Lanz <romain.lanz+github@pm.me>
2023-01-05 22:58:18 +0100

    chore: remove not used dependencies

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_GOOGLE_FONTS_API_KEY: string;
-  readonly VITE_LOGROCKET_APP_ID: string;
   readonly VITE_MERCURE_ENDPOINT: string;
   readonly VITE_SENTRY_DSN: string;
   readonly VITE_EMBED_URL: string;
