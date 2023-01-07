5a4d598ac3e38a94af52a71e23fb6ba6eded0054
Romain Lanz <romain.lanz+github@pm.me>
2023-01-07 18:49:29 +0100

    feat: mock API for development

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import './main.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '~/app';
+import { setupMockServer } from '~/boot/mock-api';
 import { setupSentry } from '~/boot/sentry';
 import AppProviders from '~/contexts/app-providers';
 
 setupSentry();
+setupMockServer();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
